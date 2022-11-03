7e9e743da81695c6afe00009fc2324f0ca3b9ecc
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-11-02 22:22:38 -0300

    Get na api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,48 @@
+import React, { useState } from 'react'
 import Button from '../components/Button'
 import EpisodiosHome from '../components/EpisodiosHome'
 import Links from '../components/Links'
-import PersonagensHome from '../components/PersonagensHome'
 import imagemRickAndMorty from './../assets/img/imagem-main-rick-and-morty.png'
+import { IoMailOutline } from 'react-icons/io5'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { FreeMode, Pagination } from 'swiper'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
 
 export default function Home() {
+  const [personagens, setPersonagens] = useState([])
+  const [episodios, setEpisodios] = useState([])
+
+  React.useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(res => res.json())
+      .then(response => {
+        let data = response.results
+        setPersonagens(data)
+        console.log(personagens)
+      })
+  }, [])
+
+  React.useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/episode')
+      .then(res => res.json())
+      .then(response => {
+        let data = response.results
+        setEpisodios(data)
+        console.log(episodios)
+      })
+  }, [])
+
   return (
-    <body>
+    <div>
       <main className="principal">
         <div>
           <p>Utilizando a API do Rick and Morty</p>
@@ -19,39 +55,74 @@ export default function Home() {
           <img src={imagemRickAndMorty} />
         </div>
       </main>
-      <section id="personagens">
+
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      <section id="personagens" className="container">
         <h2>Personagens</h2>
         <div className="div-personagens">
-          <PersonagensHome />
-          <PersonagensHome />
-          <PersonagensHome />
+          {personagens.map(personagem => (
+            <div className="card-peronsagens">
+              <img src={personagem.image} />
+              <h4>{personagem.name}</h4>
+              <span>{personagem.species}</span>
+            </div>
+          ))}
         </div>
       </section>
-      <section id="episodios">
+      <section id="episodios" className="container">
         <h2>Episodios</h2>
         <div>
-          <EpisodiosHome />
-          <EpisodiosHome />
-          <EpisodiosHome />
+          {episodios.map(episodio => (
+            <div className="div-video">
+              <h2>{episodio.name}</h2>
+              <span>{episodio.episode}</span>
+              <p>{episodio.air_date}</p>
+            </div>
+          ))}
         </div>
       </section>
       <section id="contato">
         <h2>Contato</h2>
         <ul>
           <li>
-            <i></i>
+            <i>
+              <IoMailOutline />
+            </i>
             <p>vitor_shotokan@hotmail.com</p>
           </li>
           <li>
-            <i></i>
+            <i>
+              <FaLinkedin />
+            </i>
             <p>https://www.linkedin.com/in/vitorpradodesantana/</p>
           </li>
           <li>
-            <i></i>
+            <i>
+              <FaGithubSquare />
+            </i>
             <p>https://github.com/VitorPradodeSantana</p>
           </li>
         </ul>
       </section>
-    </body>
+    </div>
   )
 }
